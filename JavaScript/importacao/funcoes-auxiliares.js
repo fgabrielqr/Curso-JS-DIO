7d251c4d9a1ab8a1cdf54efba6698f3626442f20
fgabrielqr <fgabrielqr@gmail.com>
2022-12-16 16:45:46 -0300

    feat: :zap: Funções Auxiliáres

--- JavaScript/importacao/funcoes-auxiliares.js
+++ JavaScript/importacao/funcoes-auxiliares.js
@@ -1,8 +1,13 @@
+const entradas = [5, 50, 10, 98, 23];
+let i = 0;
+
 function gets() {
-    return 10;
+    const valor = entradas[i];
+    i++;
+    return valor;
 }
 
-function print() {
+function print(texto) {
     console.log(texto);
 }
 
